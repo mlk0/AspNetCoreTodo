10b018f4319b73e0ee311f8c0338d835b60372e7
m1k0 <acme@gmx.com>
2018-09-24 01:38:31 -0400

    adding the Todo Index view and some fake data to load the view

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -2,25 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetCoreTodo.Controllers
-{
-    public class TodoController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace AspNetCoreTodo.Controllers {
+    public class TodoController : Controller {
+        public IActionResult Index () {
             // TODO: Your code here
             // Get to-do items from database
 
             // Put items into a model
 
             // Render view using the model
-
-            return Ok();
+            var todoViewModel = new TodoViewModel () {
+                Items = new TodoItem[] { new TodoItem { Id = Guid.NewGuid (), IsDone = false, Title = "Do something", DueAt = DateTimeOffset.Now } }
+            };
+            return View (todoViewModel);
         }
 
-        
-        
     }
 }
