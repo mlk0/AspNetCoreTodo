b044104b7b041a4f1d0a4457ce990234959dc9e8
m1k0 <acme@gmx.com>
2018-09-29 10:48:44 -0400

    update in the program class to call InitializeDatabase method with passing the webhost out of which the serviceScope and the serviceProvider will be initialized. Those will be needed to instantiate the identity services for creation of a role and the user

--- AspNetCoreTodo/Program.cs
+++ AspNetCoreTodo/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreTodo
@@ -14,7 +15,18 @@ namespace AspNetCoreTodo
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var webHost = CreateWebHostBuilder(args).Build();
+            InitializeDatabase(webHost);
+            webHost.Run();
+        }
+
+        private static void InitializeDatabase(IWebHost webHost)
+        {
+            using(var serviceScope = webHost.Services.CreateScope()){
+                var serviceProvider = serviceScope.ServiceProvider;
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                logger.LogDebug("in InitializeDatabase #################################");
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
